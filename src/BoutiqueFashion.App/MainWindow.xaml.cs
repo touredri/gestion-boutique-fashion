eaaa69ed29b929d2a90b4a779aeb3a75be4c1577
Drissa Toure <dcompte223@gmail.com>
2026-09-02 10:35:44 +0000

    Fix nullability warning in touch handler

--- src/BoutiqueFashion.App/MainWindow.xaml.cs
+++ src/BoutiqueFashion.App/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ public partial class MainWindow : Window
         TryOpenTouchKeyboard(e.OriginalSource as DependencyObject);
     }
 
-    private void OnWindowPreviewTouchDown(object sender, TouchEventArgs e)
+    private void OnWindowPreviewTouchDown(object? sender, TouchEventArgs e)
     {
         TryOpenTouchKeyboard(e.OriginalSource as DependencyObject);
     }
